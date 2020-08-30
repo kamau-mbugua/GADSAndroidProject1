3f75402de3d49146e92826e3ef0e6d1c95beb4fc
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-08-30 09:57:25 +0300

    first commit

---
+++ app/src/main/java/com/example/gadsassignment1/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.gadsassignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    /*Duration of Wait*/
+    private  final int SPLASH_DISPLAY_LENGTH = 3000;
+    /* Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //hide() method hides the action bar
+        getSupportActionBar().hide();
+        /*New Handler to start the Main Activity and
+         * closes this splash
+         * after some time*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //set the opening activity after 3 seconds are over
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
